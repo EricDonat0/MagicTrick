c45cf0098b1a2bfbe059b7580bb91ed3535bdb80
Eric Donato <ericdonato09@gmail.com>
2024-06-03 15:44:58 -0300

    Criação de um frmNarrador
    - Criando um narrador que permite que narre todos os acontecimentos do jogo.

---
+++ telas/frmNarrador.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace MagicTrick_piIII.Telas
+{
+    partial class frmNarrador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmNarrador));
+            this.dgvJogadores = new System.Windows.Forms.DataGridView();
+            this.gpbNarrador = new System.Windows.Forms.GroupBox();
+            this.rtbJogadas = new System.Windows.Forms.RichTextBox();
+            this.gbpJogadores = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJogadores)).BeginInit();
+            this.gpbNarrador.SuspendLayout();
+            this.gbpJogadores.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvJogadores
+            // 
+            this.dgvJogadores.AllowUserToAddRows = false;
+            this.dgvJogadores.AllowUserToDeleteRows = false;
+            this.dgvJogadores.AllowUserToResizeColumns = false;
+            this.dgvJogadores.AllowUserToResizeRows = false;
+            this.dgvJogadores.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvJogadores.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(23)))), ((int)(((byte)(31)))));
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(23)))), ((int)(((byte)(31)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(70)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvJogadores.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvJogadores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvJogadores.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvJogadores.Enabled = false;
+            this.dgvJogadores.Location = new System.Drawing.Point(6, 28);
+            this.dgvJogadores.MultiSelect = false;
+            this.dgvJogadores.Name = "dgvJogadores";
+            this.dgvJogadores.ReadOnly = true;
+            this.dgvJogadores.RowHeadersVisible = false;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(23)))), ((int)(((byte)(31)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(70)))), ((int)(((byte)(84)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.White;
+            this.dgvJogadores.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvJogadores.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgvJogadores.Size = new System.Drawing.Size(295, 176);
+            this.dgvJogadores.TabIndex = 0;
+            // 
+            // gpbNarrador
+            // 
+            this.gpbNarrador.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(23)))), ((int)(((byte)(31)))));
+            this.gpbNarrador.Controls.Add(this.rtbJogadas);
+            this.gpbNarrador.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbNarrador.ForeColor = System.Drawing.Color.White;
+            this.gpbNarrador.Location = new System.Drawing.Point(14, 229);
+            this.gpbNarrador.Name = "gpbNarrador";
+            this.gpbNarrador.Size = new System.Drawing.Size(307, 404);
+            this.gpbNarrador.TabIndex = 1;
+            this.gpbNarrador.TabStop = false;
+            this.gpbNarrador.Text = "Narrador";
+            // 
+            // rtbJogadas
+            // 
+            this.rtbJogadas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(33)))), ((int)(((byte)(46)))));
+            this.rtbJogadas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtbJogadas.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbJogadas.ForeColor = System.Drawing.Color.White;
+            this.rtbJogadas.Location = new System.Drawing.Point(6, 32);
+            this.rtbJogadas.Name = "rtbJogadas";
+            this.rtbJogadas.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.rtbJogadas.Size = new System.Drawing.Size(295, 366);
+            this.rtbJogadas.TabIndex = 0;
+            this.rtbJogadas.Text = "";
+            // 
+            // gbpJogadores
+            // 
+            this.gbpJogadores.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(23)))), ((int)(((byte)(31)))));
+            this.gbpJogadores.Controls.Add(this.dgvJogadores);
+            this.gbpJogadores.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbpJogadores.ForeColor = System.Drawing.Color.White;
+            this.gbpJogadores.Location = new System.Drawing.Point(14, 13);
+            this.gbpJogadores.Name = "gbpJogadores";
+            this.gbpJogadores.Size = new System.Drawing.Size(307, 210);
+            this.gbpJogadores.TabIndex = 2;
+            this.gbpJogadores.TabStop = false;
+            this.gbpJogadores.Text = "Jogadores";
+            // 
+            // frmNarrador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(33)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(334, 645);
+            this.Controls.Add(this.gbpJogadores);
+            this.Controls.Add(this.gpbNarrador);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmNarrador";
+            this.Text = "Narrador";
+            this.Load += new System.EventHandler(this.frmNarrador_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJogadores)).EndInit();
+            this.gpbNarrador.ResumeLayout(false);
+            this.gbpJogadores.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvJogadores;
+        private System.Windows.Forms.GroupBox gpbNarrador;
+        private System.Windows.Forms.RichTextBox rtbJogadas;
+        private System.Windows.Forms.GroupBox gbpJogadores;
+    }
+}
